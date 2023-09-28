570a8c070a957fe29b0abed25c15788504f5b58e
Radouane Rabei <rado@live.ca>
2023-09-28 13:57:51 -0400

    added files from midterm

---
+++ MVC_ShoppingCart/Controllers/ItemController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_ShoppingCart.Models;
+
+namespace MVC_ShoppingCart.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayItems() 
+        {
+            List<ItemModel> items = new List<ItemModel>
+            {
+                new ItemModel { Name = "Laptop", Quantity = 11, Price = 1500.00 },
+                new ItemModel { Name = "Desktop", Quantity = 13, Price = 1200.00 },
+                new ItemModel { Name = "Monitor", Quantity = 25, Price = 400.00 },
+                new ItemModel { Name = "Keyboard", Quantity = 15, Price = 100.00 },
+                new ItemModel { Name = "Mouse", Quantity = 30, Price = 50.00 },
+
+            };
+
+            return View(items); 
+        }
+    }
+}
---
+++ MVC_ShoppingCart/Models/ItemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_ShoppingCart.Models
+{
+    public class ItemModel
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+    }
+}
